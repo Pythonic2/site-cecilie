963e203623feb9ce7e44722f55b4e8226f349832
Igor silva <igormarinhosilva@gmail.com>
2025-03-07 23:16:19 -0300

    pasta de slides na media

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class ImagemSlide(models.Model):
-    imagem = models.ImageField(upload_to='media/slides/')
+    imagem = models.ImageField(upload_to='slides/')
     tipo = models.CharField(max_length=50, default='Evento')
 
     def __str__(self):
--- pagamento/views.py
+++ pagamento/views.py
@@ -145,13 +145,13 @@ def gerar_pagamento(cliente_id: int, produtos: list, evento: int, carrinho_id: i
     preference_data = {
         "items": items,
         "back_urls": {
-            "success": "https://choppitinerante.cloudboosterlab.org/minhas-compras/",
-            "failure": "https://choppitinerante.cloudboosterlab.org/minhas-compras/",
-            "pending": "https://choppitinerante.cloudboosterlab.org/minhas-compras/",
+            "success": "https://plataforma-donadochopp.cloudboosterlab.org/minhas-compras/",
+            "failure": "https://plataforma-donadochopp.cloudboosterlab.org/minhas-compras/",
+            "pending": "https://plataforma-donadochopp.cloudboosterlab.org/minhas-compras/",
         },
         "external_reference": f'{cliente_id}',  # Enviando o ID do usuário aqui
         "auto_return": "approved",
-        "notification_url": "https://choppitinerante.cloudboosterlab.org/pag/",
+        "notification_url": "https://plataforma-donadochopp.cloudboosterlab.org/pag/",
         "metadata": {
             "evento_id": evento,
             "carrinho_id": carrinho_id  # Passando o carrinho_id no metadata
